b0a11f0f69e12f53cdc73441a71d9c5e1f3e56c9
Yafremenka, Nadzeya <yafna01@ca.com>
2016-02-25 15:21:19 +0100

    spring boot wrks

--- rat/src/main/java/hare/displayer/view/controller/TimeLineController.java
+++ rat/src/main/java/hare/displayer/view/controller/TimeLineController.java
@@ -2,13 +2,12 @@ package hare.displayer.view.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/data")
+@RequestMapping("/hello")
 public class TimeLineController {
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping("/hh")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 }
